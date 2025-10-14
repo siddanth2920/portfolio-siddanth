9902bc5961d9b590a3717548979ef7ae2ce20945
siddanthk <konankisiddanth@gmail.com>
2025-10-14 13:25:50 +0530

    pathpdf

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -18,7 +18,7 @@ function Experience() {
   const navigate = useNavigate();
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-  const resumeFile = process.env.PUBLIC_URL + "/resumeFile.pdf";
+  const resumeFile = window.location.origin + "/resumeFile.pdf";
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
